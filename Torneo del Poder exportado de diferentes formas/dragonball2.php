acdc5c0e89ea665d3502855f6e924a9ba5424f27
lepegod <axelepegon@gmail.com>
2024-10-29 19:38:36 -0600

    Add files via upload

---
+++ Torneo del Poder exportado de diferentes formas/dragonball2.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `dragonball2`
+ */
+
+/* `dragonball2`.`characters` */
+$characters = array(
+  array('id' => '2','user_id' => '1','name' => 'axel yeray','race' => 'Saiyan','level' => '2','strength' => '54','speed' => '51','endurance' => '51','experience' => '0','avatar' => 'Avatar1.jpg'),
+  array('id' => '3','user_id' => '2','name' => 'pepe','race' => 'Freezer Race','level' => '33','strength' => '138','speed' => '154','endurance' => '146','experience' => '0','avatar' => 'Avatar10.jpg'),
+  array('id' => '4','user_id' => '3','name' => 'Gilberto','race' => 'Saiyan','level' => '122','strength' => '422','speed' => '410','endurance' => '407','experience' => '0','avatar' => 'Avatar6.jpg'),
+  array('id' => '5','user_id' => '4','name' => 'jesus','race' => 'Namek','level' => '93','strength' => '333','speed' => '305','endurance' => '318','experience' => '0','avatar' => 'Avatar3.jpg')
+);
+
+/* `dragonball2`.`character_transformations` */
+$character_transformations = array(
+);
+
+/* `dragonball2`.`tournaments` */
+$tournaments = array(
+);
+
+/* `dragonball2`.`tournament_participants` */
+$tournament_participants = array(
+);
+
+/* `dragonball2`.`transformations` */
+$transformations = array(
+);
+
+/* `dragonball2`.`users` */
+$users = array(
+  array('id' => '1','name' => 'axel yeray','email' => 'axelepegon@gmail.com','password' => '$2y$10$dIABi99Oa9OxeIU2jL0RaeJSK2fcxPoCKI26Kegikk4o2sI2tUVKS','avatar' => 'Avatar4.jpg'),
+  array('id' => '2','name' => 'jose','email' => 'pepe@gmail.com','password' => '$2y$10$s/nuTIRDQvW61h4y57nNYuwRx7WU.6fqIW85k3v9ygIA90Cc/VGhG','avatar' => 'Avatar2.jpg'),
+  array('id' => '3','name' => 'Gilberto','email' => 'lepe_79@hotmail.com','password' => '$2y$10$tn9y.Ib/KAoBdVOSGnjeJOcHYwwlucDEISb1QzJltWbWE2YZyPlRG','avatar' => 'Avatar9.jpg'),
+  array('id' => '4','name' => 'jesus alvarado','email' => 'papadio@gmail.com','password' => '$2y$10$EvwtMnFhX1zIjaWkC/qLxeuPrVy//ehNy/ti0ihdxZQLff0/qkjau','avatar' => 'Avatar9.jpg')
+);
